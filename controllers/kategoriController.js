581d250a98ff8a66c263a388a80b3f3ca9e8885a
ronaldofernandus <ronaldofernandus10@gmail.com>
2022-04-23 16:17:36 +0700

    mini-project-back-end

--- controllers/kategoriController.js
+++ controllers/kategoriController.js
@@ -4,7 +4,9 @@ class kategoriController {
   static async getKategori(req, res) {
     try {
       let getKategori = await kategori.findAll({
-          
+        order:[
+          ['id','Asc']
+        ]
       });
       res.json(getKategori);
     } catch (err) {
@@ -18,6 +20,47 @@ class kategoriController {
         namaKategori,
       });
       res.json(tambahKategori);
+    } catch (err) {
+      res.json(err);
+    }
+  }
+  static async editKategori(req, res) {
+    try {
+      const id = Number(req.params.id);
+      const { namaKategori } = req.body;
+      let editKategori = await kategori.update(
+        {
+          namaKategori
+        },
+        {
+          where: { id },
+        }
+      );
+      editKategori[0] === 1 ?
+      res.json({
+        message:`Kategori dengan id ${id} berhasil di update`
+      }):
+      res.json({
+        message:`Kategori dengan id ${id} gagal di update`
+      })
+     
+    } catch (err) {
+      res.json(err);
+    }
+  }
+  static async deleteKategori(req, res) {
+    try {
+      const id = +req.params.id;
+      let deleteKategori = await kategori.destroy({
+        where: { id },
+      });
+      deleteKategori ===1 ?
+      res.json({
+        message:`Kategori dengan id ${id} berhasil di hapus`
+      }):
+      res.json({
+        message:`Kategori dengan id ${id} tidak bisa di hapus`
+      })
     } catch (err) {
       res.json(err);
     }
--- routes/kategori.js
+++ routes/kategori.js
@@ -3,6 +3,7 @@ const { kategoriController } = require("../controllers");
 
 kategoriRoute.get("/", kategoriController.getKategori);
 kategoriRoute.post('/add',kategoriController.tambahKategori)
-kategoriRoute.post('/edit',kategoriController.editKategori)
+kategoriRoute.post('/edit/:id',kategoriController.editKategori)
+kategoriRoute.get('/delete/:id',kategoriController.deleteKategori)
 
 module.exports = kategoriRoute;
