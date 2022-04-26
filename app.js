6806b6bdacccd8c74ceaf74bc918a49c550888c7
ronaldofernandus <ronaldofernandus10@gmail.com>
2022-04-26 19:01:52 +0700

    selesai

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express =require('express')
 const route = express()
-const port=process.env.PORT ||3000
+const port=process.env.PORT || 3000
 
 
 route.use(express.json())
